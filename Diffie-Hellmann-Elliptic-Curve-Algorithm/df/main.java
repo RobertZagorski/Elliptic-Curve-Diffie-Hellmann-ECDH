ce37f9155c639e0cc579a494fbeae03144d3f261
Robert <robertzagorsky@gmail.com>
2014-03-27 23:43:52 +0100

    Pierwsza wersja projektu. Dodatkowo dorzucone materialy na temat algorytmu Diffiego-Hellmana na krzywych eliptycznych

---
+++ Diffie-Hellmann-Elliptic-Curve-Algorithm/df/main.java
@@ -0,0 +1,18 @@
+package df;
+/**
+ * 
+ * @author Vitali Karpinski
+ * @author Robert Zagórski
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
